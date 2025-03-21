e245973f0355db63df5c43d7d4e0137f94628357
DongHun Choi <a3989957@naver.com>
2025-03-21 18:59:03 +0900

    MODIFY : RedisConfig

--- src/main/java/com/mysite/stockburning/configuration/RedisConfig.java
+++ src/main/java/com/mysite/stockburning/configuration/RedisConfig.java
@@ -15,7 +15,7 @@ public class RedisConfig {
 
     @Bean //Redis 서버와 포트 설정
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory("13.125.234.34", 6379);
+        return new LettuceConnectionFactory("54.180.150.253", 6379);
     }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
